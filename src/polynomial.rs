dc25ad912d6f3f7b55f0c6071f736ca97a7a8639
anthonyoneill <ajoneill@protonmail.com>
2023-11-28 16:41:38 +0000

    Polynomial roots + addition

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -1,7 +1,7 @@
-use core::ops::{Index, IndexMut};
+use core::ops::{Index, IndexMut, Add};
 //use core::ops::{Neg, Add, Sub, Mul, Div};
 use crate::{ Cmplx, Vector };
-use crate::traits::Zero;
+use crate::traits::{Zero, Number};
 
 pub struct Polynomial<T> {
     /// The coefficients of the polynomial
@@ -40,10 +40,11 @@ impl<T> Polynomial<T> {
         self.coeffs.len()
     }
 
-    /// Return the degree of the polynomial
+    /// Return the degree of the polynomial or an error if < 1.
     #[inline]
-    pub fn degree(&self) -> usize {
-        self.coeffs.len() - 1
+    pub fn degree(&self) -> Result<usize, &'static str> {
+        if self.coeffs.len() == 0 { Err("Polynomial.degree() == 0.") }
+        else { Ok( self.coeffs.len() - 1 ) }
     }
 
     /// Return the coefficients of the polynomial as a mutable reference
@@ -58,63 +59,122 @@ impl<T> Polynomial<T> {
     where
         T: Copy + core::ops::Mul<Output = T> + core::ops::Add<Output = T>,
     {
-        let mut p = self.coeffs[ self.degree() ];
-        for i in (0..self.degree()).rev() {
+        let degree = self.degree().unwrap(); //TODO unwrap
+        let mut p = self.coeffs[ degree ];
+        for i in (0..degree).rev() {
             p = p * x + self.coeffs[ i ];
         }
         p
     }
 
-    
-
-    /// Divide the polynomial by another polynomial to get a quotient and remainder
-    #[inline]
-    pub fn polydiv( &self, _v: &Polynomial<T> ) -> ( Polynomial<T>, Polynomial<T> ) {
-        //TODO
-        ( Polynomial::empty(), Polynomial::empty() )
-    }
-
-    //TODO polydiv, solve/roots, derivative (and integral?) use Luna Polynomial class as a guide
+    //TODO polydiv, derivative (and integral?) use Luna Polynomial class as a guide
 
 }
+
+impl<T: Clone> Clone for Polynomial<T> {
+    /// Clone the polynomial
+    #[inline]
+    fn clone(&self) -> Self {
+        Self::new( self.coeffs.clone() )
+    }
+}
+
+impl<T: Copy + Clone + Number> Add<Polynomial<T>> for Polynomial<T> {
+    type Output = Self;
+    /// Add two polynomials together ( binary + )
+    #[inline]
+    fn add(self, plus: Self) -> Self::Output {
+        let mut sum = Polynomial::<T>::empty();
+        let mut degree = match self.degree() {
+            Ok( d ) => d,
+            Err( _ ) => { return plus.clone(); },
+        };
+        let plus_degree = match plus.degree() {
+            Ok( d ) => d,
+            Err( _ ) => { return self.clone(); },
+        };
+        if degree < plus_degree {
+            degree = plus_degree;
+        }
+        sum.coeffs = vec![ T::zero(); degree + 1 ];
+        for i in 0..=degree {
+            if i <= self.degree().unwrap() {
+                sum.coeffs[ i ] = sum.coeffs[ i ] + self.coeffs[ i ].clone();
+            }
+            if i <= plus.degree().unwrap() {
+                sum.coeffs[ i ] = sum.coeffs[ i ] + plus.coeffs[ i ].clone();
+            }
+        }
+        sum
+    }
+}
+
+//TODO subtraction, multiplication, division (quotient and remainder)
 
 impl Polynomial<f64> {
     /// Find all the roots of the polynomial with f64 coefficients
     /// refine = true will refine the roots using Laguerre's method
     #[inline]
-    pub fn roots( &self, _refine: bool ) -> Vector::<Cmplx> {
-        let mut poly_roots = Vector::<Cmplx>::zeros( self.degree() );
-        if self.degree() == 0 { panic!( "Polynomial roots error: degree must be at least one.") }
-        if self.degree() == 1 {
-            poly_roots[0] = Cmplx::new( - self.coeffs[0] / self.coeffs[1], 0.0 );
-            //return poly_roots;
+    pub fn roots( &self, refine: bool ) -> Vector::<Cmplx> {
+        let mut coeffs: Vector::<Cmplx> = Vector::new( self.coeffs.len(), Cmplx::zero() );
+        for i in 0..self.coeffs.len() {
+            coeffs[i] = Cmplx::new( self.coeffs[i], 0.0 ); // Convert to Complex<f64>
         }
-        if self.degree() == 2 {
-            let a = Cmplx::new( self.coeffs[2], 0.0 );
-            let b = Cmplx::new( self.coeffs[1], 0.0 );
-            let c = Cmplx::new( self.coeffs[0], 0.0 );
-            poly_roots = Polynomial::quadratic_solve( a, b, c );
-            //return poly_roots;
-        }
-        if self.degree() == 3 {
-            let a = Cmplx::new( self.coeffs[3], 0.0 );
-            let b = Cmplx::new( self.coeffs[2], 0.0 );
-            let c = Cmplx::new( self.coeffs[1], 0.0 );
-            let d = Cmplx::new( self.coeffs[0], 0.0 );
-            println!( "a = {}, b = {}, c = {}, d = {}", a, b, c, d );
-            poly_roots = Polynomial::cubic_solve( a, b, c, d );
-            //return poly_roots;
-        }
+        Polynomial::<Cmplx>::poly_solve( coeffs, refine )
+    }
 
+    /// Divide the polynomial by another polynomial to get a quotient and remainder
+    #[inline]
+    pub fn polydiv( &self, v: &Polynomial<f64> ) -> Result<( Polynomial<f64>, Polynomial<f64> ), &'static str> {
+        let mut n = self.degree()?;
+        let mut nv = v.degree()? as isize;
+        while ( nv >= 0 ) && ( v.coeffs[ nv as usize ] == 0.0 ) { nv -= 1; }
+        if nv < 0 { return Err( "Polynomial.polydiv() divide by zero polynomial" ); }
+        
+        let mut r = Polynomial::<f64>::new( self.coeffs.clone() );
+        let mut q = Polynomial::<f64>::empty();
+        q.coeffs = vec![ 0.0; n - nv as usize + 1 ];
 
-        poly_roots
+        for k in (0..=n-nv as usize).rev() {
+            q.coeffs[ k ] = r.coeffs[ nv as usize + k ] / v.coeffs[ nv as usize ];
+            for j in (k..=nv as usize).rev() {
+                r.coeffs[ j ] -= q.coeffs[ k ] * v.coeffs[ j - k ];
+            }
+        }
+        for j in (nv as usize)..=n {
+            r.coeffs[ j ] = 0.0;
+        }
+        // Remove leading zeros from r and q polynomials
+        let mut r_val = r.coeffs[ n ];
+        while r_val.abs() == 0.0 && n > 0 {
+            r.coeffs.pop();
+            n -= 1;
+            r_val = r.coeffs[ n ];
+        }
+        n = self.degree()? - nv as usize;
+        let mut q_val = q.coeffs[ n ];
+        while q_val.abs() == 0.0 && n > 0 {
+            q.coeffs.pop();
+            n -= 1;
+            q_val = q.coeffs[ n ];
+        }
+        Ok( ( q, r ) )
     }
 
 }
 
 impl Polynomial<Cmplx> {
+    /// Find all the roots of the polynomial with Complex<f64> coefficients
+    /// refine = true will refine the roots using Laguerre's method
+    #[inline]
+    pub fn roots( &self, refine: bool ) -> Vector::<Cmplx> {
+        let mut coeffs: Vector::<Cmplx> = Vector::new( self.coeffs.len(), Cmplx::zero() );
+        for i in 0..self.coeffs.len() { coeffs[i] = self.coeffs[i]; }
+        Polynomial::<Cmplx>::poly_solve( coeffs, refine )
+    }
+
     // Solve the quadratic equation ax^2 + bx + c = 0
-    fn quadratic_solve( a: Cmplx, b: Cmplx, c: Cmplx ) -> Vector<Cmplx> {
+    fn quadratic_solve( a: Cmplx, b: Cmplx, c: Cmplx ) -> Vector::<Cmplx> {
         let mut roots = Vector::<Cmplx>::zeros( 2 );
         let discriminant: Cmplx = b * b - a * c * 4.0;
         let mut sgn: f64 = ( b.conj() * discriminant.sqrt() ).real;
@@ -127,18 +187,20 @@ impl Polynomial<Cmplx> {
     }
 
     // Solve the cubic equation ax^3 + bx^2 + cx + d = 0
-    fn cubic_solve( a: Cmplx, b: Cmplx, c: Cmplx, d: Cmplx ) -> Vector<Cmplx> {
+    fn cubic_solve( a: Cmplx, b: Cmplx, c: Cmplx, d: Cmplx ) -> Vector::<Cmplx> {
         let mut roots = Vector::<Cmplx>::zeros( 3 );
-        let disc =  a * b * c * d * 18.0 - b * b * b * d * 4.0 + b * b * c * c - a * c * c * c * 4.0 - a * a * d * d * 27.0;
-        let d0 = b * b - a * c * 3.0;
-        let d1 = b * b * b * 2.0 - a * b * c * 9.0 + a * a * d * 27.0;
+        let ( a2, b2, c2, d2 ) = ( a * a, b * b, c * c, d * d );
+        let dis = a * b * c * d * 18. - b * b2 * d * 4. + b2 * c2 - a * c2 * c * 4. - a2 * d2 * 27.;
+        let d0 = b2 - a * c * 3.;
+        let d1 = b2 * b * 2. - a * b * c * 9. + a2 * d * 27.;
         if d0 == Cmplx::zero() && d1 == Cmplx::zero() { // 3 equal roots
             roots[0] = -b / ( a * 3.0 );
             roots[1] = roots[0];
             roots[2] = roots[0];
         } else {
-            let sqrt = (-a * a * disc * 27.0).sqrt();
-            let k = ( if d1 < Cmplx::zero() { d1 - sqrt } else { d1 + sqrt } / 2.0).pow( &Cmplx::new( 1. / 3.0, 0.0 ) );
+            let sqrt = (-a * a * dis * 27.0).sqrt();
+            let base = if d1 < Cmplx::zero() { d1 - sqrt } else { d1 + sqrt } / 2.0;
+            let k = base.pow( &Cmplx::new( 1. / 3.0, 0.0 ) );
             roots[0] = -(b + k + d0 / k) / ( a * 3.0 );
             let u = Cmplx::new( -0.5, (3.0_f64).sqrt() / 2.0 );
             roots[1] = -(b + u * k + d0 / ( u * k ) ) / ( a * 3.0 );
@@ -147,9 +209,105 @@ impl Polynomial<Cmplx> {
         }
         roots
     }
-}
 
-//TODO impl Polynomial<Cmplx> 
+    // Find all the roots of the polynomial with Complex<f64> coefficients
+    // refine = true will refine the roots using Laguerre's method
+    #[inline]
+    fn poly_solve( coeffs: Vector::<Cmplx>, refine: bool ) -> Vector::<Cmplx> {
+        let degree = coeffs.size() - 1;
+        let mut poly_roots = Vector::<Cmplx>::zeros( degree );
+        if degree == 0 { panic!( "Polynomial roots error: degree must be at least one.") }
+        if degree == 1 {
+            poly_roots[0] = - coeffs[0] / coeffs[1];
+        }
+        if degree == 2 {
+            let a = coeffs[2];
+            let b = coeffs[1];
+            let c = coeffs[0];
+            poly_roots = Polynomial::quadratic_solve( a, b, c );
+        }
+        if degree == 3 {
+            let a = coeffs[3];
+            let b = coeffs[2];
+            let c = coeffs[1];
+            let d = coeffs[0];
+            poly_roots = Polynomial::cubic_solve( a, b, c, d );
+        }
+        let eps = f64::EPSILON;
+        let mut its: usize = 0;
+        let mut a = coeffs.clone();
+        if degree > 3 {
+            let mut ad = coeffs.clone();
+            let mut b;
+            for j in (0..degree).rev() {
+                let mut x = Cmplx::zero();
+                let mut ad_v = Vector::<Cmplx>::zeros( j + 2 );
+                for jj in 0..j+2 {
+                    ad_v[jj] = ad[jj];
+                }
+                Self::laguer( &mut ad_v, &mut x, &mut its );
+                if x.imag.abs() <= 2.0 * eps * x.real.abs() {
+                    x = Cmplx::new( x.real, 0.0 );
+                }
+                poly_roots[j] = x;
+                b = ad[ j + 1 ];
+                for jj in (0..j+1).rev() {
+                    let c = ad[jj];
+                    ad[jj] = b;
+                    b = x * b + c;
+                }
+            }
+        }
+        if refine {
+            for j in 0..degree {
+                Self::laguer( &mut a, &mut poly_roots[j], &mut its );
+            }
+        }
+        poly_roots
+    }
+
+    fn laguer( a: &mut Vector::<Cmplx>, x: &mut Cmplx, iterations: &mut usize ) {
+        const MR: usize = 8;
+        const MT: usize = 10;
+        const MAXIT: usize = MT * MR;
+        const EPS: f64 = f64::EPSILON;
+        let frac: [f64; MR + 1] = [ 0.0,0.5,0.25,0.75,0.13,0.38,0.62,0.88,1.0 ];
+        let m = a.size() - 1;
+        for iter in 1..MAXIT {
+            *iterations = iter;
+            let mut b = a[m];
+            let mut err = b.abs();
+            let mut d = Cmplx::zero();
+            let mut f = Cmplx::zero();
+            let abx = x.abs();
+            for j in (0..m).rev() {
+                f = *x * f + d;
+                d = *x * d + b;
+                b = *x * b + a[j];
+                err = b.abs() + abx * err;
+            }
+            err *= EPS;
+            if b.abs() <= err { return; }
+            let g = d / b;
+            let g2 = g * g;
+            let h = g2 - ( f / b ) * 2.0;
+            let sq = ( ( h * (m as f64) - g2 ) * ( m - 1 ) as f64  ).sqrt();
+            let mut gp = g + sq;
+            let gm = g - sq;
+            let abp = gp.abs();
+            let abm = gm.abs();
+            if abp < abm { gp = gm; }
+            let dx = if f64::max( abp, abm ) > 0.0 { 
+                Cmplx::new( m as f64, 0.0 ) / gp
+            } else {
+                Cmplx::polar( 1.0 + abx, iter as f64 )
+            };
+            let x1 = *x - dx;
+            if *x == x1 { return; }
+            if iter % MT != 0 { *x = x1; } else { *x -= dx * frac[ iter / MT ]; }
+        }
+    }
+}
 
 impl<T> Index<usize> for Polynomial<T> {
     type Output = T;
--- tests/polynomial/polynomial.rs
+++ tests/polynomial/polynomial.rs
@@ -1,4 +1,4 @@
-use ohsl::{Polynomial, Cmplx};
+use ohsl::{Polynomial, Cmplx, One};
 
 #[test]
 fn unspecified_polynomial() {
@@ -15,7 +15,7 @@ fn specified_polynomial() {
     assert_eq!( p[2], 3.0 );
     p[2] = 4.0;
     assert_eq!( p[2], 4.0 );
-    assert_eq!( p.degree(), 2 );
+    assert_eq!( p.degree().unwrap(), 2 );
     assert_eq!( p.coeffs(), &vec![ 1.0, 2.0, 4.0 ] );
     *p.coeffs() = vec![ 1.0, 1.0, 1.0 ];
     assert_eq!( p.coeffs(), &vec![ 1.0, 1.0, 1.0 ] );
@@ -33,7 +33,7 @@ fn evaluate_polynomial() {
 fn quadratic_polynomial() {
     let mut p = Polynomial::quadratic( 1.0, 2.0, 4.0 ); // x^2 + 2x + 4
     assert_eq!( p.size(), 3 );
-    assert_eq!( p.degree(), 2 );
+    assert_eq!( p.degree().unwrap(), 2 );
     assert_eq!( p[0], 4.0 );
     assert_eq!( p[1], 2.0 );
     assert_eq!( p[2], 1.0 );
@@ -41,20 +41,18 @@ fn quadratic_polynomial() {
     assert_eq!( p.evaluate( 2.0 ), 12.0 );
     let roots = p.roots( true );
     assert_eq!( roots.size(), 2 );
-    let r0 = Cmplx::new( -1.0, -(3.0_f64).sqrt() );
-    let r1 = Cmplx::new( -1.0,  (3.0_f64).sqrt() );
-    let eps = 1.0e-15; //TODO use f64::EPSILON when refinement is implemented
-    assert!( ( roots[0].real - r0.real ).abs() < eps );
-    assert!( ( roots[0].imag - r0.imag ).abs() < eps );
-    assert!( ( roots[1].real - r1.real ).abs() < eps );
-    assert!( ( roots[1].imag - r1.imag ).abs() < eps );
+    let r0 = Cmplx::new( -1.0, -1.732050807568877293527446 );
+    let r1 = Cmplx::new( -1.0,  1.732050807568877293527446 );
+    let eps = 2.0 * f64::EPSILON;
+    assert!( ( roots[0] - r0 ).abs() < eps );
+    assert!( ( roots[1] - r1 ).abs() < eps );
 }
 
 #[test]
 fn cubic_polynomial() {
     let mut p = Polynomial::cubic( 1.0, 2.0, 3.0, 4.0 ); // x^3 + 2x^2 + 3x + 4
     assert_eq!( p.size(), 4 );
-    assert_eq!( p.degree(), 3 );
+    assert_eq!( p.degree().unwrap(), 3 );
     assert_eq!( p[0], 4.0 );
     assert_eq!( p[1], 3.0 );
     assert_eq!( p[2], 2.0 );
@@ -64,16 +62,13 @@ fn cubic_polynomial() {
     assert_eq!( p.evaluate( 2.0 ), 26.0 );
     let roots = p.roots( true );
     assert_eq!( roots.size(), 3 );
-    let r0 = Cmplx::new( -1.65062919143939, 0.0 );
-    let r1 = Cmplx::new( -0.17468540428031, -1.54686888723140 );
-    let r2 = Cmplx::new( -0.17468540428031,  1.54686888723140 );
-    let eps = 1.0e-14; //TODO use f64::EPSILON when refinement is implemented
-    assert!( ( roots[0].real - r0.real ).abs() < eps );
-    assert!( ( roots[0].imag - r0.imag ).abs() < eps );
-    assert!( ( roots[1].real - r1.real ).abs() < eps );
-    assert!( ( roots[1].imag - r1.imag ).abs() < eps );
-    assert!( ( roots[2].real - r2.real ).abs() < eps );
-    assert!( ( roots[2].imag - r2.imag ).abs() < eps );
+    let r0 = Cmplx::new( -1.650629191439388218880801, 0.0 );
+    let r1 = Cmplx::new( -0.174685404280305890559600, -1.546868887231396277142806 );
+    let r2 = Cmplx::new( -0.174685404280305890559600,  1.546868887231396277142806 );
+    let eps = 2. * f64::EPSILON;
+    assert!( ( roots[0] - r0 ).abs() < eps );
+    assert!( ( roots[1] - r1 ).abs() < eps );
+    assert!( ( roots[2] - r2 ).abs() < eps );
 }
 
 #[test]
@@ -84,14 +79,156 @@ fn cubic_repeated_roots() {
     let r0 = Cmplx::new( -1.0, 0.0 );
     let r1 = Cmplx::new( -1.0, 0.0 );
     let r2 = Cmplx::new( -1.0, 0.0 );
-    let eps = 1.0e-14; //TODO use f64::EPSILON when refinement is implemented
-    assert_eq!( roots[0], r0 );
-    assert!( ( roots[0].real - r0.real ).abs() < eps );
-    assert!( ( roots[0].imag - r0.imag ).abs() < eps );
-    assert!( ( roots[1].real - r1.real ).abs() < eps );
-    assert!( ( roots[1].imag - r1.imag ).abs() < eps );
-    assert!( ( roots[2].real - r2.real ).abs() < eps );
-    assert!( ( roots[2].imag - r2.imag ).abs() < eps );
+    let eps = 2. * f64::EPSILON;
+    assert!( ( roots[0] - r0 ).abs() < eps );
+    assert!( ( roots[1] - r1 ).abs() < eps );
+    assert!( ( roots[2] - r2 ).abs() < eps );
 }
 
 //TODO need more testing of cubic_solve
+
+#[test]
+fn quartic_polynomial() {
+    let p = Polynomial::new( vec![ 5.0, 4.0, 3.0, 2.0, 1.0 ] ); // x^4 + 2x^3 + 3x^2 + 4x + 5
+    assert_eq!( p.degree().unwrap(), 4 );
+    assert_eq!( p.evaluate( 1.0 ), 15.0 );
+    assert_eq!( p.evaluate( -1.0 ), 3.0 );
+    let roots = p.roots( true );
+    assert_eq!( roots.size(), 4 );
+    let r0 = Cmplx::new( -1.287815479557647988872498, -0.8578967583284902864164198 );
+    let r1 = Cmplx::new( -1.287815479557647988872498,  0.8578967583284902864164198 );
+    let r2 = Cmplx::new(  0.287815479557647988872498, -1.416093080171907938724658 );
+    let r3 = Cmplx::new(  0.287815479557647988872498,  1.416093080171907938724658 );
+    let eps = 2. * f64::EPSILON;
+    assert!( ( roots[0] - r0 ).abs() < eps );
+    assert!( ( roots[1] - r1 ).abs() < eps );
+    assert!( ( roots[2] - r2 ).abs() < eps );
+    assert!( ( roots[3] - r3 ).abs() < eps );
+}
+
+#[test]
+fn complex_quadratic() { // (1+i)x^2 + x + 1
+    let p = Polynomial::quadratic( Cmplx::new( 1.0, 1.0 ), Cmplx::one(), Cmplx::one() );
+    assert_eq!( p.degree().unwrap(), 2 );  
+    assert_eq!( p.evaluate( Cmplx::one() ), Cmplx::new( 3.0, 1.0 ) );
+    let roots = p.roots( true );
+    assert_eq!( roots.size(), 2 );
+    let r0 = Cmplx::new( 0.0, 1.0 );
+    let r1 = Cmplx::new( -0.5, -0.5 );
+    let eps = 2. * f64::EPSILON;
+    assert!( ( roots[0] - r0 ).abs() < eps );
+    assert!( ( roots[1] - r1 ).abs() < eps );
+}
+
+#[test]
+fn complex_cubic() { // (1+i)x^3 + x^2 + x + 1
+    let p = Polynomial::cubic( Cmplx::new( 1.0, 1.0 ), Cmplx::one(), Cmplx::one(), Cmplx::one() );
+    assert_eq!( p.degree().unwrap(), 3 );
+    assert_eq!( p.evaluate( Cmplx::one() ), Cmplx::new( 4.0, 1.0 ) );
+    let roots = p.roots( true );
+    assert_eq!( roots.size(), 3 );
+    let r0 = Cmplx::new( -0.7744803530832973567204905,  0.2599691354027557701265485 );
+    let r1 = Cmplx::new( -0.0932651775266529118468203, -0.7875537459442802686603073 );
+    let r2 = Cmplx::new(  0.3677455306099502685673108,  1.0275846105415244985337588 );
+    let eps = 2. * f64::EPSILON;
+    assert!( ( roots[0] - r0 ).abs() < eps );
+    assert!( ( roots[1] - r1 ).abs() < eps );
+    assert!( ( roots[2] - r2 ).abs() < eps );
+}
+
+#[test]
+fn polydiv_divide_by_empty() {
+    let u = Polynomial::new( vec![ 1.0, 2.0, 3.0 ] ); // 1 + 2x + 3x^2
+    let v = Polynomial::empty();
+    let result = u.polydiv( &v );
+    assert!( result.is_err() );
+}
+
+#[test]
+fn polydiv_divide_by_zero() {
+    let u = Polynomial::new( vec![ 1.0, 2.0, 3.0 ] ); // 1 + 2x + 3x^2
+    let v = Polynomial::new( vec![ 0.0, 0.0, 0.0 ] );
+    let result = u.polydiv( &v );
+    assert!( result.is_err() );
+}
+
+#[test]
+fn polydiv_divide_by_one() {
+    let u = Polynomial::new( vec![ 1.0, 2.0, 3.0 ] ); // 1 + 2x + 3x^2
+    let v = Polynomial::new( vec![ 1.0 ] );
+    let result = u.polydiv( &v );
+    assert!( result.is_ok() );
+    let ( q, r ) = result.unwrap();
+    assert_eq!( q.degree().unwrap(), 2 );
+    assert_eq!( q[0], 1.0 );
+    assert_eq!( q[1], 2.0 );
+    assert_eq!( q[2], 3.0 );
+    assert_eq!( r.degree().unwrap(), 0 );
+    assert_eq!( r[0], 0.0 );
+}
+
+#[test]
+fn polydiv() {
+    let u = Polynomial::new( vec![ 1.0, 1.0, 1.0 ] ); // 1 + x + x^2
+    let v = Polynomial::new( vec![ 1.0, 1.0 ] ); // 1 + x
+    let result = u.polydiv( &v );
+    assert!( result.is_ok() );
+    let ( q, r ) = result.unwrap();
+    assert_eq!( q.degree().unwrap(), 1 );
+    assert_eq!( q[0], 0.0 ); // q = x
+    assert_eq!( q[1], 1.0 );
+    assert_eq!( r.degree().unwrap(), 0 );
+    assert_eq!( r[0], 1.0 ); // r = 1
+}
+
+/*#[test]
+fn polydiv_cubic() {
+    let u = Polynomial::cubic( 1.0, 0.0, 0.0, -1.0 ); // x^3 - 1
+    let v = Polynomial::new( vec![ -1.0, 1.0 ] ); // -1 + x
+    let result = u.polydiv( &v );
+    assert!( result.is_ok() );
+    let ( mut q, mut r ) = result.unwrap();
+    assert_eq!( q.degree().unwrap(), 2 );
+    println!( "q = {:?}", *q.coeffs() );
+    //assert_eq!( q[0], 1.0 ); // q = x^2 + x + 1
+    //assert_eq!( q[1], 1.0 );
+    //assert_eq!( q[2], 1.0 );
+    assert_eq!( r.degree().unwrap(), 0 );
+    println!( "r = {:?}", *r.coeffs() );
+    assert_eq!( r[0], 0.0 ); // r = 0
+}*/
+
+#[test]
+fn polynomial_addition_zero() {
+    let p = Polynomial::new( vec![ 1.0, 2.0, 3.0 ] ); // 1 + 2x + 3x^2
+    let zero = Polynomial::empty();
+    let s = zero.clone() + p.clone();
+    assert_eq!( s.degree().unwrap(), 2 );
+    assert_eq!( s[0], 1.0 );
+    assert_eq!( s[1], 2.0 );
+    assert_eq!( s[2], 3.0 );
+    let t = p + zero;
+    assert_eq!( t.degree().unwrap(), 2 );
+    assert_eq!( t[0], 1.0 );
+    assert_eq!( t[1], 2.0 );
+    assert_eq!( t[2], 3.0 );
+}
+
+#[test]
+fn polynomial_addition() {
+    let p = Polynomial::new( vec![ 1.0, 2.0, 3.0 ] ); // 1 + 2x + 3x^2
+    let q = Polynomial::new( vec![ 4.0, 5.0, 6.0, 7.0 ] ); // 4 + 5x + 6x^2 + 7x^3
+    let r = p.clone() + q;
+    assert_eq!( r.degree().unwrap(), 3 );
+    assert_eq!( r[0], 5.0 );
+    assert_eq!( r[1], 7.0 );
+    assert_eq!( r[2], 9.0 );
+    assert_eq!( r[3], 7.0 );
+    let zero = Polynomial::empty();
+    let s = p + zero;
+    assert_eq!( s.degree().unwrap(), 2 );
+    assert_eq!( s[0], 1.0 );
+    assert_eq!( s[1], 2.0 );
+    assert_eq!( s[2], 3.0 );
+}
+
